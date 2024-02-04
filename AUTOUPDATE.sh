83a9e54d4f9e6971acc7d04393a453340201c77d
henmel1 <henrymellor04@gmail.com>
2024-02-04 01:36:54 +0000

    update

--- AUTOUPDATE.sh
+++ AUTOUPDATE.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname ${BASH_SOURCE[0]})
 
-if [[ -n $(git status) ]]; then
+if [[ -n $(git status -s) ]]; then
     echo "Changes found. Pulling changes..."
     git add -A && git commit -m 'update' && git pull --rebase
     cp index.html /var/www/html
