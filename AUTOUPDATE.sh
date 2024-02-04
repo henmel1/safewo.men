b9b364b190a828695003094ae206f582c60d25fd
henmel1 <henrymellor04@gmail.com>
2024-02-04 01:44:02 +0000

    update

--- AUTOUPDATE.sh
+++ AUTOUPDATE.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
 cd $(dirname ${BASH_SOURCE[0]})
-
-if [[ -n $(git status -s) ]]; then
-    echo "Changes found. Pulling changes..."
-    git add -A && git commit -m 'update' && git pull --rebase
-    cp index.html /var/www/html
-    service apache2 restart
-else
-    echo "No changes found. Skip pulling."
-fi
+git add -A && git commit -m 'update' && git pull
+cp index.html /var/www/html
