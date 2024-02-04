1857091cf30c35e25bff0cd67d2045efe4eb2795
henmel1 <henrymellor04@gmail.com>
2024-02-04 00:51:02 +0000

    update

--- AUTOUPDATE.sh
+++ AUTOUPDATE.sh
@@ -3,8 +3,8 @@
 cd $(dirname ${BASH_SOURCE[0]})
 
 if [[ -n $(git status -s) ]]; then
-    echo "Changes found. Pushing changes..."
-    git add -A && git commit -m 'update' && git push
+    echo "Changes found. Pulling changes..."
+    git add -A && git commit -m 'update' && git pull
 else
     echo "No changes found. Skip pushing."
 fi
